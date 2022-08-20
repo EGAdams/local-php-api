4237420cafea38d9edd22849e457c46e1d8c956c
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-20 16:57:42 -0400

    reformatted

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -1,9 +1,8 @@
 <?php
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
- 
-class UserModel extends Database
-{
-    public function getUsers( $limit ) {
-        return $this->select( "SELECT * FROM users ORDER BY user_id ASC LIMIT ?", [ "i", $limit ]);
-    }
+
+class UserModel extends Database {
+	public function getUsers($limit) {
+		return $this->select("SELECT * FROM users ORDER BY user_id ASC LIMIT ?", ["i", $limit]);
+	}
 }
