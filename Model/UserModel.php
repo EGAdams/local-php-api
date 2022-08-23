8d17281a7df4870982d27517a6a99184b39507d9
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-22 20:12:51 -0400

    formatted.

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -2,10 +2,10 @@
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 
 class UserModel extends Database {
-	public function getMonitoredObjects() {return $this->select( "SELECT * FROM monitored_objects" );}
+	public function getMonitoredObjects() { return $this->select( "SELECT * FROM monitored_objects" ); }
 
-	public function insertObject($object_view_id, $object_data) {
-		return $this->insert("INSERT INTO monitored_objects ( object_view_id, object_data )
+	public function insertObject( $object_view_id, $object_data ) {
+		return $this->insert( "INSERT INTO monitored_objects ( object_view_id, object_data )
                                VALUES ( " . "'" . $object_view_id . "', '" . $object_data . "')");
 	}
 }
