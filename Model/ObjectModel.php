4d987ec749876f65f70c8fa8d90baa0c8930fcfa
egadams <31349790+EGAdams@users.noreply.github.com>
2022-09-04 16:51:45 -0400

    commented interface declarations.  fixed update()

--- Model/ObjectModel.php
+++ Model/ObjectModel.php
@@ -1,7 +1,7 @@
 <?php
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 
-class ObjectModel extends Database implements ObjectInserter, ObjectUpdater, ObjectDeleter, ObjectSelector {
+class ObjectModel extends Database /* implements ObjectInserter, ObjectUpdater, ObjectDeleter, ObjectSelector */ {
 
     public function __construct( $table_arg ) { $this->table = $table_arg   ; }
                                                 
@@ -16,7 +16,7 @@ class ObjectModel extends Database implements ObjectInserter, ObjectUpdater, Obj
                                VALUES ( $object_view_id, $object_data )" ); }
 
     public function updateObject( $object_view_id, $object_data ) {
-        return $this->updateObject( 
+        return $this->update( 
             "  UPDATE $this->table " .
             "  SET object_data='" . $object_data .
             "' WHERE object_view_id='" . $object_view_id . "'" ); }
