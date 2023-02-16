4f2dffb1143992ecd20b66e7554ec078e5a0a21b
adamsl <eg1972@gmail.com>
2023-02-16 12:19:05 -0500

    fixed FileLogger location

--- Model/Database.php
+++ Model/Database.php
@@ -1,24 +1,15 @@
 <?php
-// echo "require_once config.php... <br>";
 require_once  __DIR__  . "/../inc/config.php";
-// echo "done requiring config.php. <br>";
 class Database {
     protected $connection = null;
 	public function __construct() {
         try {
-            // echo "connecting to database ... <br>";
-            // echo DB_HOST . "<br>" . DB_USERNAME . "<br>" . DB_PASSWORD . "<br>" . DB_DATABASE_NAME . "<br>";
-            
-            $this->connection = new mysqli( DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME );
-            // echo "done making connection. <br>";
+            $this->connection = new mysqli( MO_DB_HOST, MO_DB_USERNAME, MO_DB_PASSWORD, MO_DB_DATABASE_NAME );
 			if ( mysqli_connect_errno()) { throw new DatabaseException( "Could not connect to database." ); }
 		} catch ( Exception $e ) {
 		    echo "*** ERROR: DatabaseException about to be thrown! *** <br>";
-		    echo "\$e->getMessage(): " . $e->getMessage();
-         
-		    throw new DatabaseException( $e->getMessage()); }
-	        // echo "done constructing Database object. <br>";
-	}
+		    echo "\$e->getMessage(): " . $e->getMessage();         
+		    throw new DatabaseException( $e->getMessage()); }}
     
 	public function select( $query = "", $params = []) {
         try {
@@ -27,21 +18,17 @@ class Database {
 			$stmt->close();
 			return $result;
 		} catch ( Exception $e ) { throw new DatabaseException( $e->getMessage()); }
-		return false;
-	}
+		return false; }
     
     public function insert( $query ) {
         try {
-            // echo "query: " . $query . "<br>";
-            
             $stmt = $this->executeStatement( $query );
             $stmt->close();
             return $stmt;
         } catch ( Exception $e ) {
             echo "*** ERROR: DatabaseException: ".$e->getMessage() ."<br>";
             throw new DatabaseException( $e->getMessage()); }
-        return false;
-    }
+        return false; }
 
     public function delete( $query ) {
         try {
@@ -49,8 +36,7 @@ class Database {
             $stmt->close();
             return $stmt;
         } catch ( Exception $e ) { throw new DatabaseException( $e->getMessage()); }
-        return false;
-    }
+        return false; }
 
     public function update( $query, $params = []) {
         try {
@@ -58,8 +44,7 @@ class Database {
             $stmt->close();
             return $stmt;
         } catch ( Exception $e ) { throw new DatabaseException( $e->getMessage()); }
-        return false;
-    }
+        return false; }
     
 	private function executeStatement( $query = "", $params = []) {
 		try {
@@ -68,9 +53,6 @@ class Database {
 			if ( $params ) { $stmt->bind_param( $params[ 0 ], $params[ 1 ]); }
 			$stmt->execute();
 			return $stmt;
-		} catch ( Exception $e ) { throw new DatabaseException( $e->getMessage()); }
-	}
-}
-class DatabaseException extends Exception {} // hush the generic Exception warning
+		} catch ( Exception $e ) { throw new DatabaseException( $e->getMessage()); }}}
 
-// echo "done with Database definition. <br>";
+class DatabaseException extends Exception {} // hush the generic Exception warning
--- Model/ObjectModel.php
+++ Model/ObjectModel.php
@@ -1,38 +1,34 @@
 <?php
-// define("PROJECT_ROOT_PATH", "/home/adamsl/joomla/prototype/local-php-api" );
-// echo "requiring Database.php... <br>";
 
+require_once  __DIR__  . "/../inc/bootstrap.php";
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
-require_once dirname(__DIR__, 1) . "/local-php-api/FileLogger.php";
-// echo "done requiring database. <br>";
+require_once PROJECT_ROOT_PATH . "/FileLogger.php";
 
 class ObjectModel extends Database/* implements ObjectInserter, ObjectUpdater, ObjectDeleter, ObjectSelector */ {
-
-	public function __construct($table_arg) {
-		// echo "constructing Database parent... <br>";
+    private $table;
+	public function __construct( $table_arg) {
 		parent::__construct();
-		// echo "done constructing ObjectModel parent. <br>";
 		$this->table = $table_arg;}
 
-	public function selectAllObjects() {return $this->select("SELECT * FROM $this->table");}
+	public function selectAllObjects() { return $this->select( "SELECT * FROM $this->table" );}
 
-	public function selectObject($object_view_id) {
-		return $this->select("SELECT * FROM $this->table WHERE object_view_id='" . $object_view_id . "'");}
+	public function selectObject( $object_view_id) {
+		return $this->select( "SELECT * FROM $this->table WHERE object_view_id='" . $object_view_id . "'" );}
 
-	public function deleteObject($object_view_id) {return $this->delete(
-		"DELETE FROM $this->table WHERE object_view_id='" . $object_view_id . "'");}
+	public function deleteObject( $object_view_id) { return $this->delete(
+		"DELETE FROM $this->table WHERE object_view_id='" . $object_view_id . "'" );}
 
-	public function insertObject($object_view_id, $object_data) {
+	public function insertObject( $object_view_id, $object_data) {
+        FileLogger::writeLog( __METHOD__, "inserting id: " . $object_view_id . " with data size:" . sizeof( $object_data ));
 		try {
-			$this->insert("INSERT INTO $this->table ( object_view_id, object_data )
-                               VALUES ( '$object_view_id', '$object_data' )");
-		} catch (Exception $e) {return json_encode("*** ERROR: " . $e->getMessage . " ***");}
-		return json_encode("insert success.");}
+			$this->insert( "INSERT INTO $this->table ( object_view_id, object_data )
+                               VALUES ( '$object_view_id', '$object_data' )" );
+		} catch ( Exception $e ) { return json_encode( "*** ERROR: " . $e->getMessage() . " ***" );}
+		return json_encode( "insert success." );}
 
-	public function updateObject($object_view_id, $object_data) {
+	public function updateObject( $object_view_id, $object_data) {
         FileLogger::writeLog( __METHOD__, "updating id: " . $object_view_id . " with data:" . $object_data  );
 		return $this->update(
 			"UPDATE $this->table " . "  SET object_data='" . $object_data .
-			"' WHERE object_view_id='" . $object_view_id . "'");}
+			"' WHERE object_view_id='" . $object_view_id . "'" );}
 }
-// echo "done with object model definition. <br>";
